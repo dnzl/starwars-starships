7f275d7a86d92929966822d0b1a39b641e62a4bb
dnzl <dnzl@dnzl.com.ar>
2019-07-09 20:14:25 +0200

    Updates

---
+++ precache-manifest.845dc17c7029b61c29405c24535a09e6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7d4907cc670474827da5a7ee2c548a6",
+    "url": "/starwars-starships/index.html"
+  },
+  {
+    "revision": "173f6c98c2dcf1f876ae",
+    "url": "/starwars-starships/static/css/main.21bc7d61.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/starwars-starships/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "173f6c98c2dcf1f876ae",
+    "url": "/starwars-starships/static/js/main.0faa07aa.chunk.js"
+  },
+  {
+    "revision": "caec8f0775e9fc40cdef",
+    "url": "/starwars-starships/static/js/runtime~main.bfe8377b.js"
+  }
+]);
